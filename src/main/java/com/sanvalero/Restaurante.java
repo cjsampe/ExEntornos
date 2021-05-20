30b7385d61ea1beb1a0410406b74e6a75f08e14d
cjsampe <samperiz@hotmail.com>
2021-05-20 19:26:25 +0100

    clase resturante

---
+++ src/main/java/com/sanvalero/Restaurante.java
@@ -0,0 +1,42 @@
+
+package com.sanvalero;
+
+/**
+ *
+ * @author María Carmen Jiménez Sampériz
+ */
+public class Restaurante {
+    private String nombreRestaurante;
+    private String direccionResta;
+    private int numeroMesas;
+
+    public Restaurante() {
+    }
+
+    public String getNombreRestaurante() {
+        return nombreRestaurante;
+    }
+
+    public void setNombreRestaurante(String nombreRestaurante) {
+        this.nombreRestaurante = nombreRestaurante;
+    }
+
+    public String getDireccionResta() {
+        return direccionResta;
+    }
+
+    public void setDireccionResta(String direccionResta) {
+        this.direccionResta = direccionResta;
+    }
+
+    public int getNumeroMesas() {
+        return numeroMesas;
+    }
+
+    public void setNumeroMesas(int numeroMesas) {
+        this.numeroMesas = numeroMesas;
+    }
+
+    
+    
+}
