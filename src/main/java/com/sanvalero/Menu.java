fefded9307307b35e6f3301e2b0dcef2d8a55ed5
cjsampe <samperiz@hotmail.com>
2021-05-20 19:32:17 +0100

    subo la clase menu

---
+++ src/main/java/com/sanvalero/Menu.java
@@ -0,0 +1,43 @@
+
+package com.sanvalero;
+
+/**
+ *
+ * @author María Carmen Jiménez Sampériz
+ */
+public class Menu {
+    private String plato;
+    private String ingrediente;
+    private boolean disponible;
+
+    public Menu() {
+    }
+
+    public String getPlato() {
+        return plato;
+    }
+
+    public void setPlato(String plato) {
+        this.plato = plato;
+    }
+
+    public String getIngrediente() {
+        return ingrediente;
+    }
+
+    public void setIngrediente(String ingrediente) {
+        this.ingrediente = ingrediente;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
+    
+    
+    
+}
